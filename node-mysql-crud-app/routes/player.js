19e94b73d8e7e550df4b99e195609f8ec742177d
apham1594 <88216187+apham1594@users.noreply.github.com>
2022-03-22 04:21:41 -0600

    cannot find 'stack' i can't find the error

---
+++ node-mysql-crud-app/routes/player.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const express = require("express");
+
+const router = express.Router();
+
+const playerController = require('../controllers/players.controller')
+
+http.createServer(require('stack')(
+router.get('/add', playerController.addPlayerPage);
+router.get('/edit/:id', playerController.editPlayerPage);
+router.get('/delete/:id', playerController.deletePlayer);
+
+router.post('/add', playerController.addPlayer);
+router.post('/edit/:id', playerController.editPlayer);
+
+module.exports = router;
